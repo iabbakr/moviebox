f22cfbda64cddf9f3c6a0a5e3b74e7debd1b6c84
iabbakr <abubakaribrahim603@gmail.com>
2023-09-16 19:58:39 +0100

    added card

---
+++ src/components/Card.jsx
@@ -0,0 +1,254 @@
+import React from "react"
+import Abu from './image/Abubakar.png'
+
+
+
+export default function Card() {
+    return (
+        <section className="cards-list">
+        
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+            <div className="card">
+                <img 
+                    src={Abu} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>ratin</span>
+                    <span className="gray">stats </span>
+                    <span className="gray">location</span>
+                </div>
+                <p className="card--title">jdfd</p>
+                <p className="card--price">
+                    <span className="bold">fdjhbvhjfvf</span> / person
+                </p>
+            </div>
+        </section>
+        
+    )
+}
